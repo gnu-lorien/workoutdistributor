be9e2d488afc6d6b745a6a3f1e35fad81dae963e
Andrew Sayman <andrew.sayman@gmail.com>
2022-07-02 19:00:13 -0400

    Become a Red cog

---
+++ workoutdistributor/workoutdistributor.py
@@ -0,0 +1,22 @@
+from typing import Literal
+
+import discord
+from redbot.core import commands
+from redbot.core.bot import Red
+from redbot.core.config import Config
+
+RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
+
+
+class WorkoutDistributor(commands.Cog):
+    def __init__(self, bot: Red) -> None:
+        self.bot = bot
+        self.config = Config.get_conf(
+            self,
+            identifier="WorkoutDistributor",
+            force_registration=True,
+        )
+
+    async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
+        # TODO: Replace this with the proper end user data removal handling.
+        super().red_delete_data_for_user(requester=requester, user_id=user_id)

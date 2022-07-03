89b73c7b846a71d3321242676b48ccbcd1768ac1
Andrew Sayman <andrew.sayman@gmail.com>
2022-07-02 21:56:01 -0400

    Fixup some of the bot stuff

--- workoutdistributor/workoutdistributor.py
+++ workoutdistributor/workoutdistributor.py
@@ -10,13 +10,14 @@ RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 
 class WorkoutDistributor(commands.Cog):
     def __init__(self, bot: Red) -> None:
+        super().__init__()
         self.bot = bot
         self.config = Config.get_conf(
             self,
-            identifier="WorkoutDistributor",
+            identifier=992835661398818846,
             force_registration=True,
         )
 
     async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
         # TODO: Replace this with the proper end user data removal handling.
-        super().red_delete_data_for_user(requester=requester, user_id=user_id)
+        await super().red_delete_data_for_user(requester=requester, user_id=user_id)

ce5359bfbbe42b9af4aa4426ada1e8a32c5a97f8
Andrew Sayman <andrew.sayman@gmail.com>
2023-03-07 22:34:26 -0500

    Load properly without redbot

--- workoutdistributor/__init__.py
+++ workoutdistributor/__init__.py
@@ -1,13 +1,16 @@
 import json
 from pathlib import Path
 
-from redbot.core.bot import Red
+try:
+    from redbot.core.bot import Red
 
-from .workoutdistributor import WorkoutDistributor
+    from .workoutdistributor import WorkoutDistributor
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+    with open(Path(__file__).parent / "info.json") as fp:
+        __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-async def setup(bot: Red) -> None:
-    bot.add_cog(WorkoutDistributor(bot))
+    async def setup(bot: Red) -> None:
+        bot.add_cog(WorkoutDistributor(bot))
+except ImportError:
+    pass
